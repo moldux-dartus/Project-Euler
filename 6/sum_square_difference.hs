603f92ad2e05ca7b78257a434813de6681890e31
Moldux Dartus <moldux-dartus@protonmail.com>
2022-03-06 16:39:24 -0600

    prettified 6

--- 6/sum_square_difference.hs
+++ 6/sum_square_difference.hs
@@ -1,9 +1,18 @@
-square x = x * x
-
-squareOfSum n = square $ sum [1..n]
-
-sumOfSquares n = sum $ map square [1..n]
-
-showAnswer = (squareOfSum 100) - sumOfSquares 100
-
+--square x = x * x
+--
+--squareOfSum n = square $ sum [1..n]
+--
+--sumOfSquares n = sum $ map square [1..n]
+--
+--showAnswer = (squareOfSum 100) - sumOfSquares 100
+--
 --First try, ugly but don't care
+
+showAnswer = squareOfSum - sumOfSquares
+  where
+    squareOfSum :: Int
+    squareOfSum = (^2) $ sum [1..100]
+    sumOfSquares :: Int
+    sumOfSquares = sum $ map (^2) [1..100]
+
+    
